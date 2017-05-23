18599110ef48554b3306523518138e540d63daba
Bram Verboom <bramv44@gmail.com>
2017-05-23 19:40:57 +0200

    Fix annoying border

--- src/main/Main.java
+++ src/main/Main.java
@@ -39,10 +39,11 @@ public class Main extends Canvas implements Runnable{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
 		frame.add(this, BorderLayout.CENTER);
-		frame.pack();
 		frame.setResizable(false);
+		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		
 	}
 	
 	public void init(){
